11462f615b88ea43cc6ba8f5eb41fe0ad9a724ba
JoeyP <Joey.don0905@gmail.com>
2019-07-11 16:51:16 +1000

    finish comments

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -6,7 +6,14 @@ currentPage: the current page index.
 pageSize: How many card to display in a page (Load from PAGE_SIZE in ../settings/setting)
 
 Algorithm:
-Extract
+1. Calculate the right *card* start index in the array.
+e.g. if current page is 2, page size is 3 x 4 =12. The start index will be 13 and the first card is data[13].
+
+2. Calculate the right *card* last index in the array.
+e.g. if current page is 2, page size is 3 x 4 =12. The start index will be 24 and the first card is data[24].
+
+3. Extract this part of array from the whole cached data array and return a new array
+e.g [data[13],data[14],data[15],data[16], ...data[24]] for the page 2
 */
 export const paginatify = (items, currentPage, pageSize) => {
   let startIndex = (currentPage - 1) * pageSize;
